a22f598c361b6d201dac3a8bfc69c9b7c810c863
Anjum Faraz <anjumfaraz10@gmail.com>
2020-05-17 11:48:45 -0500

    feat: customizing the gatsby config and content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Personal Blog`,
+    author: `A-Faraz`,
+    description: `A personal blog.`,
+    siteUrl: `https://personal-blog-af.netlify.app`,
     social: {
-      twitter: `kylemathews`,
+      twitter: ``,
     },
   },
   plugins: [
@@ -77,7 +77,7 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         // edit below
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/af-logo.jpg`,
       },
     },
     {
